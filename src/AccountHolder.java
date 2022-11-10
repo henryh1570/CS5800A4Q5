f1c2f10ab6ffa71f20f8d26d89312990fa08d096
henryh1570 <henryhu346@gmail.com>
2022-11-10 15:52:58 -0800

    Added other class files

--- src/AccountHolder.java
+++ src/AccountHolder.java
@@ -1,2 +1,15 @@
-public class AccountHolder {
+import java.util.concurrent.ThreadLocalRandom;
+
+public abstract class AccountHolder {
+    protected int id;
+    protected String address;
+
+    public AccountHolder(int id, String address) {
+        this.id = id;
+        this.address = address;
+    }
+
+    public int getNextID() {
+        return ThreadLocalRandom.current().nextInt(999999) + 1;
+    }
 }
---
+++ src/IndividualHolder.java
@@ -0,0 +1,27 @@
+public class IndividualHolder extends AccountHolder {
+    private String name;
+    private String SSN;
+
+
+    public IndividualHolder(int id, String address, String name, String SSN) {
+        super(id, address);
+        this.name = name;
+        this.SSN = SSN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+}
