a8ab183d195b827d767b0add0659f8d1cd9f7196
henryh1570 <henryhu346@gmail.com>
2022-11-10 15:37:33 -0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * CS5800 - Advanced Software Engineering
+ * Assignment 4, Question 5
+ * Desc: Given a UML class diagram, this program implements it by providing the corresponding
+ *      java files with their expected code behaviors. Upload this to an online repository.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
